915c3a1bc60112e1fe9d656146788858eeece8e1
pashutaz <pazaytsev@yahoo.com>
2016-05-16 15:25:47 +0300

    Добавил индексатор

--- labaN4/Program.cs
+++ labaN4/Program.cs
@@ -2,51 +2,65 @@
 
 namespace labaN4
 {
+  internal interface IHuman
+  {
+    string ShowInfo();
+  }
+
+  internal class Human : IHuman
+  {
+    private string[] _properties;
+
+    public string this[int index]
+    {
+      set
+      {
+        Array.Resize(ref _properties,index+1);
+        _properties[index] = value;
+      }
+
+      get { return _properties[index]; }
+    }
+
+    public string Name { get; set; }
+
+    public string Job { get; set; }
+
+    public string Address { get; set; }
+
+    public string Birthday { get; set; }
+
+    public Human(string name = "", string job = "", string address = "", string birthday = "")
+    {
+      _properties = new string[] {};
+      Name = name;
+      Job = job;
+      Address = address;
+      Birthday = birthday;
+    }
+
+    public string ShowInfo()
+    {
+      return Name + ":\n " + Address + "\n " + Job + "\n " + Birthday + "\n ";
+    }
+  }
+
   internal class MainClass
   {
-    private interface IHuman
-    {
-      string GetName();
-      string GetBirthday();
-      string GetAddress();
-      string GetJob();
-    }
-
-    private class Human : IHuman
-    {
-
-
-      private string _name;
-      private string _birthday;
-      private string _address;
-      private string _job;
-
-
-
-      public void SetHuman(string inputName, string inputBirthday, string inputAddress, string inputJob)
-            {
-              _name = inputName;
-              _birthday = inputBirthday;
-              _address = inputAddress;
-              _job = inputJob;
-            }
-
-      public string GetName() => _name;
-
-      public string GetBirthday() => _birthday;
-
-      public string GetAddress() => _address;
-
-      public string GetJob() => _job;
-    }
-
-
     public static void Main(string[] args)
     {
-      var person1 = new Human();
-      person1.SetHuman("pavel", "10/04/95", "moscow", "engineer");
-
-      Console.WriteLine(person1.GetName());
+      var person1 = new Human
+      {
+        Name = "Pavel",
+        Job = "Engineer",
+        Address = "Moscow",
+        Birthday = "27/06/95",
+        [5] = "Hello",
+        [7] = "user",
+      };
+      Console.WriteLine(person1.ShowInfo());
+      Console.WriteLine(person1[5]+", "+person1[7]);
     }
   }
 }
+//DONE
